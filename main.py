a6ffd8c610b820be76cad50db08ba4670d26ee9f
GUAN MING <guanmingchiu@gmail.com>
2024-08-16 06:37:26 +0800

    env: setup cors middleware

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.main import api_router
 
 # FastAPI app
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8081",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router)
