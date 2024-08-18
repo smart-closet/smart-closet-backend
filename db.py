b9725702ec784fbba675f3910eb2af574cf2ff3c
GUAN MING <guanmingchiu@gmail.com>
2024-08-18 22:46:53 +0800

    feat: add code

--- db.py
+++ db.py
@@ -11,5 +11,5 @@ def get_session():
     with Session(engine) as session:
         yield session
 
-# SQLModel.metadata.drop_all(engine)
+SQLModel.metadata.drop_all(engine)
 # SQLModel.metadata.create_all(engine)
